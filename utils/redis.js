8099b2df9b4ba00f8fcdb56d37d742ecead3b6fd
onokajames96 <jamesonoka996@gmail.com>
2024-05-13 06:32:26 -0700

    redis clienty

--- utils/redis.js
+++ utils/redis.js
@@ -12,3 +12,35 @@ class RedisClient {
 isAlive() {
 	this.client.connected;
 }
+import { createClient } from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+  constructor() {
+    this.myClient = createClient();
+    this.myClient.on('error', (error) => console.log(error));
+  }
+
+  isAlive() {
+    return this.myClient.connected;
+  }
+
+  async get(key) {
+    const getAsync = promisify(this.Client.GET).bind(this.Client);
+    return getAsync(key);
+  }
+
+  async set(key, val, time) {
+    const setAsync = promisify(this.Client.SET).bind(this.Client);
+    return setAsync(key, val, 'EX', time);
+  }
+
+  async del(key) {
+    const delAsync = promisify(this.Client.DEL).bind(this.Client);
+    return delAsync(key);
+  }
+}
+
+const redisClient = new RedisClient();
+
+export default redisClient;
